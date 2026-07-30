feb032477783c213410b5409b718645bbd79ab69
pallavi S <1ep22ic038@eastpoint.ac.in>
2026-07-30 12:45:14 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+      System.out.println("Welcome to the java Track");
+      System.out.println("i already know how to build programming logic");
+      System.out.println("Now i am learning how to express that logic using java");
     }
 }
