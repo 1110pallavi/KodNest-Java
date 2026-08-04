278ef19ef81afad5f534f446522b30ac5e5039f7
pallavi S <1ep22ic038@eastpoint.ac.in>
2026-08-04 11:42:22 +0530

    days

---
+++ track/practiceM01/Main1.java
@@ -0,0 +1,11 @@
+package track.practiceM01;
+
+public class Main1 {
+    public static void main(String[] args){
+        String LN = "Ananya";
+        int CP = 25;
+        double PH = 
+
+    }
+
+}
