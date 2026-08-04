283b4a359fcbbed3b3be6bf1c8e2d146767db94a
pallavi S <1ep22ic038@eastpoint.ac.in>
2026-08-04 12:09:20 +0530

    day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++
@@ -1,8 +0,0 @@
-public class WelcomeToJava {
-  public static void main(String[] args) {
-    System.out.println("Welcome to the java Track");
-    System.out.println("i already know how to build programming logic");
-    System.out.println("Now i am learning how to express that logic using java");
-    System.out.println("Now i know how to use git  and github");
-  }
-}
--- track/M02/pgm1.java
+++
@@ -1,8 +0,0 @@
-package track.M02;
-
-public class pgm1 {
-    public static void main(String[] args) {
-        System.out.println("Hello KodNest");
-    }
-
-}
