7a9b01c5704efde94d7c8a6d4aef4cc9faf58b7d
1110pallavi <arunasriram395@gmail.com>
2026-08-05 14:18:30 +0530

    pratice

---
+++ track/M02/pratice9.java
@@ -0,0 +1,18 @@
+package track.M02;
+
+public class pratice9 {
+
+    public static void main(String[] args) {
+
+        int marks = 72;
+        int attendance = 80;
+        boolean projectCompleted = true;
+
+        boolean marksEligible = marks >= 60;
+        boolean attendanceEligible = attendance >= 75;
+        boolean academicEligible = marksEligible && attendanceEligible;
+        boolean placementEligible = academicEligible && projectCompleted;
+
+        System.out.println("Placement Eligible: " + placementEligible);
+    }
+}
